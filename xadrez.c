d75bbf15abc95a487af0290441e2ebda9ae1942f
Margaret Santos de Oliveira <margaretsantos.adv@outlook.com>
2025-05-23 17:57:26 +0000

    Correção de erros

--- xadrez.c
+++ xadrez.c
@@ -5,13 +5,12 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-     printf("Movimento da Torre (5 casas para a direita):\n");
+    printf("Movimento da Torre (5 casas para a direita):\n");
     for (int i = 1; i <= 5; i++) {
         printf("Direita\n");
-   
-}
+   }
 
-printf("\nMovimento do Bispo (5 casas na diagonal: Cima Direita):\n");
+    printf("\nMovimento do Bispo (5 casas na diagonal: Cima Direita):\n");
     int j = 1;
     while (j <= 5) {
         printf("Cima Direita\n");
